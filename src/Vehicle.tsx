d4098342f5ddcf49b11e851964088fd9853eb380
natgo <xenhud@protonmail.com>
2022-08-16 16:09:32 +0300

    Update

--- src/Vehicle.tsx
+++ src/Vehicle.tsx
@@ -1,11 +1,25 @@
 import { useParams } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 
 import "./App2.css";
+import { Final } from "./atom";
 import { SpecsCard } from "./VehicleItem";
+
+function QueryVehicle(vehicleId:string) {
+  const FinalValue = useRecoilValue(Final);
+  console.log(vehicleId);
+  console.log(vehicleId.replace("_"," ").replace("_"," "));
+  const matchAir = FinalValue.aircraft.find((curval) => {
+    return curval.wikiname.toLowerCase() === vehicleId.replace("_"," ").replace("_"," ").toLowerCase();
+  });
+  return matchAir;
+}
 
 export function Vehicle(): JSX.Element {
   const params = useParams();
   console.log(params);
+  const match = QueryVehicle(params.vehicleId);
+  console.log(match);
   return (
     <SpecsCard
       intname={"b_48_firecrest"}
