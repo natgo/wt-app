1239fa0c99dcac6026080d8ee6cfbbdda1f87e71
natgo <xenhud@protonmail.com>
2023-02-02 14:27:25 +0200

    Update compare

--- src/pages/Compare/Compare.tsx
+++ src/pages/Compare/Compare.tsx
@@ -5,9 +5,11 @@ import { Fab } from "@mui/material";
 
 import { SearchDialog } from "@/components/Techtree/SearchDialog";
 import { SpecsCard } from "@/components/Vehicle/VehicleItem";
+import { Optics } from "@/components/Vehicle/VehicleOptics";
+import { GroundWeapon } from "@/components/Vehicle/VehicleWeapon";
 import { compareVehicleState } from "@/store/compare";
 import { searchDialogState } from "@/store/search";
-import { VehicleProps } from "@/types";
+import { GroundProps, VehicleProps } from "@/types";
 
 export default function Compare(): JSX.Element {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -27,15 +29,24 @@ export default function Compare(): JSX.Element {
         display: "grid",
         gridAutoFlow: "column",
         gridAutoColumns: "1fr",
-        justifyItems: "center",
       }}
     >
-      {compare.map((vehicle) => (
-        <div key={vehicle.intname}>
-          <SpecsCard vehicle={vehicle} />
-          <CompareFunction vehicle={vehicle} />
-        </div>
-      ))}
+      <div
+        style={{
+          display: "grid",
+          gridAutoFlow: "column",
+          gridAutoColumns: "1fr",
+          justifyItems: "center",
+        }}
+      >
+        {compare.map((vehicle) => (
+          <div key={vehicle.intname}>
+            <SpecsCard vehicle={vehicle} />
+          </div>
+        ))}
+      </div>
+      <CompareFunction vehicles={compare} />
+
       <Fab
         color="primary"
         sx={{ position: "absolute", bottom: "16px", left: "16px" }}
@@ -48,14 +59,34 @@ export default function Compare(): JSX.Element {
   );
 }
 
-function CompareFunction(params: { vehicle: VehicleProps }) {
-  const { vehicle } = params;
-  if (vehicle.type === "tank") {
+function CompareFunction(params: { vehicles: VehicleProps[] }) {
+  const { vehicles } = params;
+  if (vehicles.length > 0 && vehicles[0].type === "tank") {
+    const vehicles2 = vehicles as GroundProps[];
     return (
-      <div>
-        <div>Mas: {vehicle.mass / 1000}t</div>
-        <div>Horsepower: {vehicle.horsepower}</div>
-        <div>Power to weight: {(vehicle.horsepower / (vehicle.mass / 1000)).toFixed(1)} hp/t</div>
+      <div
+        style={{
+          display: "grid",
+          gridAutoFlow: "column",
+          gridAutoRows: "1fr",
+          gridRow: 2,
+        }}
+      >
+        {vehicles2.map((vehicle) => (
+          <>
+            <div>
+              <div>Mas: {vehicle.mass / 1000}t</div>
+              <div>Horsepower: {vehicle.horsepower}</div>
+              <div>
+                Power to weight: {(vehicle.horsepower / (vehicle.mass / 1000)).toFixed(1)} hp/t
+              </div>
+              <GroundWeapon vehicle={vehicle} />
+            </div>
+            <div style={{ gridRow: 2 }}>
+              <Optics vehicle={vehicle} />
+            </div>
+          </>
+        ))}
       </div>
     );
   }
