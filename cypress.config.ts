eca51c2700b66021492819c6b7e3a03feab0af43
natgo <xenhud@protonmail.com>
2023-10-26 17:14:34 +0300

    Increase cypress timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   viewportHeight: 960,
   viewportWidth: 1536,
+  defaultCommandTimeout: 7000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
